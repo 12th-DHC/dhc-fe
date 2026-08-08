4c51f39fe3837324a72af41c93a520a08b45f39b
JANG SEONG JU <printhelloworld0702@gmail.com>
2026-08-08 17:30:34 +0900

    feat(user_pwa): 설정 페이지 제목 추가

--- apps/user_pwa/src/pages/Setting.tsx
+++ apps/user_pwa/src/pages/Setting.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar"
+import TitleBar from "../components/TitleBar"
 import { FullCenteredBox } from "../styles/Box.style"
 
 function SettingPage() {
   return (
     <FullCenteredBox>
+      <TitleBar title={"설정"} description={"이메일, 호실 비밀번호를 설정하세요."} />
       <Navbar selected={"setting"} />
     </FullCenteredBox>
   )
